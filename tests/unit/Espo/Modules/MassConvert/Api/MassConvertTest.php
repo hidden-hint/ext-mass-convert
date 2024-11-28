e258d933d3523dcf45e24c8172543dfcf0bfe470
tim-bezhashvyly <462687+tim-bezhashvyly@users.noreply.github.com>
2024-11-28 12:55:34 +0100

    Fix MassConvertTest

--- tests/unit/Espo/Modules/MassConvert/Api/MassConvertTest.php
+++ tests/unit/Espo/Modules/MassConvert/Api/MassConvertTest.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace Espo\Modules\MassConvert\Api;
 
 use Espo\Core\Api\Request;
+use Espo\Core\Exceptions\BadRequest;
+use Espo\Core\Exceptions\Conflict;
+use Espo\Core\Exceptions\Error;
+use Espo\Core\Exceptions\Forbidden;
+use Espo\Core\Exceptions\NotFound;
+use Espo\Core\Select\SelectBuilderFactory;
 use Espo\Modules\MassConvert\Lead\MassConvertService;
+use Espo\ORM\EntityManager;
+use JsonException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,6 +21,14 @@ use PHPUnit\Framework\TestCase;
  */
 final class MassConvertTest extends TestCase
 {
+    /**
+     * @throws BadRequest
+     * @throws Forbidden
+     * @throws Conflict
+     * @throws Error
+     * @throws NotFound
+     * @throws JsonException
+     */
     public function testDelegatesConversionToServiceClass(): void
     {
         $testIds = ['1', '2', '3'];
@@ -23,7 +39,12 @@ final class MassConvertTest extends TestCase
         $mockService = $this->createMock(MassConvertService::class);
         $mockService->expects($this->once())->method('convert')->with(...$testIds);
 
-        $action = new MassConvert($mockService);
+        $action = new MassConvert(
+            $mockService,
+            $this->createStub(SelectBuilderFactory::class),
+            $this->createStub(EntityManager::class)
+        );
+
         $response = $action->process($stubRequest);
 
         $this->assertSame('true', (string) $response->getBody());
